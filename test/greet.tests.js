8bef000046d9396612785b43fe287d0dc4a60c55
tumisobrian27426 <78339704+tumisobrian27426@users.noreply.github.com>
2021-04-14 13:40:33 +0200

    intial commit

---
+++ test/greet.tests.js
@@ -0,0 +1,13 @@
+describe('Test my greet function' , function(){
+    it('it should return "Hello, Brian" when called with "Brian"' , function(){
+        assert.equal('Hello, Brian', greet("Brian"), "this should not be true");
+
+    });
+ describe('Test my greet function' , function(){
+    it('it should return "Hello, Andre" when called with "Andre"' , function(){
+        assert.equal('Hello, Andre', greet("Andre"), "this should not be true");
+    
+        });
+    });
+
+});
